89e51912c7c19c2b0b5adafcb7f00616ff77192a
butonG21 <73821573+butonG21@users.noreply.github.com>
2023-10-20 17:42:14 +0000

    edit detail comp

--- src/scripts/component/restaurant-detail/detail-restaurant.js
+++ src/scripts/component/restaurant-detail/detail-restaurant.js
@@ -1,23 +1,27 @@
 import API_ENDPOINTS from '../../globals/api-endpoints';
 
 class RestaurantDetail extends HTMLElement {
-  set Detail(detail) {
+  set detail(detail) {
     this._detail = detail;
     this.render();
   }
 
   render() {
-    this.innerHTML = `
-        <div class="content">
-            <div class="restaurant-img">
-                <img
-                    src="${API_ENDPOINTS.Image_Url.Medium_Resolution(this._detail.picttureId)}"
-                    alt="resto-profile"
-                />
-            </div>
+    if (!this._detail) {
+      this.innerHTML = '<p>Loading...</p>';
+      return;
+    }
 
-        </div>
-        `;
+    this.innerHTML = `
+      <div class="content">
+          <div class="restaurant-img">
+              <img
+                  src="${API_ENDPOINTS.Image_Url.Medium_Resolution(this._detail.pictureId)}"
+                  alt="resto-profile"
+              />
+          </div>
+      </div>
+    `;
   }
 }
 customElements.define('restaurant-detail', RestaurantDetail);
--- src/scripts/views/pages/Detail.js
+++ src/scripts/views/pages/Detail.js
@@ -1,23 +1,32 @@
 import UrlParser from '../../routes/url-parser';
 import RestaurantDataSources from '../../data/restaurant-source';
-import {createElementDetail} from '../templates/template-creator'
+import '../../component/restaurant-detail/detail-restaurant';
+import { createElementDetail } from '../templates/template-creator';
 
-class Detail {
+const Detail = {
   async render() {
     return `
     <div class="container" id='main-content'>
-      <resto-detail></resto-detail>
+      <restaurant-detail></restaurant-detail>
     </div>
     `;
-  }
+  },
 
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
 
-    const resto = (await RestaurantSource.getRestaurantsDetail(url.id))
-        .restaurant;
+    try {
+      const restaurantData = await RestaurantDataSources.detailsRestaurant(url.id);
+      console.log(restaurantData)
+      if (restaurantData.restaurant) {
+        createElementDetail('restaurant-detail', restaurantData.restaurant);
+      } else {
+        console.log('Restaurant data is not available.');
+      }
+    } catch (error) {
+      console.error('Error fetching restaurant data:', error);
+    }
+  },
+};
 
-    createElementDetail('resto-detail', resto);
-}
-}
 export default Detail;
