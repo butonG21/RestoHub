41f095fef0e07591bc734e6de4c0179eb9f04484
butonG21 <73821573+butonG21@users.noreply.github.com>
2023-10-22 00:54:50 +0000

    changes component using lit

--- src/scripts/component/restaurant-detail/detail-restaurant.js
+++ src/scripts/component/restaurant-detail/detail-restaurant.js
@@ -1,35 +1,95 @@
-import './restaurant-info';
+import { html, css, LitElement } from 'lit';
+import './restaurant-info/restaurant-info';
 
-class RestaurantDetail extends HTMLElement {
-  set Detail(detail) {
-    this._detail = detail;
-    this.render();
+class RestaurantDetail extends LitElement {
+  static styles = css`
+
+  .hero-background .background-wrap{
+    width: 100%;
+    height: 450px;
+    position: relative;
+    z-index: 0;
+    left: 0;
+}
+
+.hero-background .background-wrap img{
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+}
+
+.hero-background .background-wrap .gradient-img{
+   position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+        90deg,
+    rgba(0, 0, 0, 0.6) 0%,
+    rgba(76, 76, 76, 0.5) 56.39%,
+    rgba(196, 196, 196, 0.15) 100%);
+}
+@media screen and (min-width: 320px) {
+  .hero-background .background-wrap {
+    width: 100%;
+    height: 420px;
+
+    position: relative;
+    z-index: 0;
+    left: 0;
   }
 
+  .hero-background .background-wrap img{
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+
+  .hero-background .background-wrap .gradient-img{
+    position: absolute;
+      width: 100%;
+      height: 100%;
+
+      background: linear-gradient(
+          90deg,
+          rgba(0, 0, 0, 0.6) 0%,
+          rgba(76, 76, 76, 0.5) 100%
+      );
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .hero-background .background-wrap {
+    height: 320px;
+  }
+}
+`;
+
+  static properties = {
+    Detail: { type: Object },
+  };
+
   render() {
-    if (!this._detail) {
-      this.innerHTML = '<p>Loading...</p>';
-      return;
+    if (!this.Detail) {
+      return html`<p>Loading...</p>`;
+    }
+    const restaurantInfo = this.querySelector('restaurant-info');
+    if (restaurantInfo) {
+      restaurantInfo.info = this.Detail;
     }
 
-    this.innerHTML = `
+    return html`
       <div class="hero-background">
         <div class="background-wrap">
           <div class="gradient-img"></div>
-              <img
-                  src="./images/heros/hero-image_4.jpg"
-                  alt="restaurant
-          -profile"
-              />
+          <img
+            src="./images/heros/hero-image_4.jpg"
+            alt="restaurant-profile"
+          />
         </div>
       </div>
-      <restaurant-info></restaurant-info>
+      <restaurant-info .info="${this.Detail}"></restaurant-info>
     `;
-
-    const restaurantInfo = this.querySelector('restaurant-info');
-    if (restaurantInfo) {
-      restaurantInfo.Info = this._detail;
-    }
   }
 }
+
 customElements.define('restaurant-detail', RestaurantDetail);
---
+++ src/scripts/component/restaurant-detail/restaurant-info/restaurant-info.js
@@ -0,0 +1,44 @@
+import { html, css, LitElement } from 'lit';
+import API_ENDPOINTS from '../../../globals/api-endpoints';
+
+class RestaurantInfo extends LitElement {
+  static styles = css`
+    /* CSS styles for the component */
+  `;
+
+  static properties = {
+    info: { type: Object },
+  };
+
+  render() {
+    return html`
+      <div class="content">
+        <div class="restaurant-image">
+          <img src="${API_ENDPOINTS.Image_Url.Small_Resolution(this.info.pictureId)}" alt="Profile Image" />
+        </div>
+
+        <div class="restaurant-info">
+          <h2 class="name">${this.info.name}</h2>
+          <div class="location-rating">
+            <div class="location">
+              <img src="./images/location-sign2.svg" alt="location-sign" />
+              <span class="location-name">${this.info.city}</span>
+            </div>
+            <div class="rating">
+              <img src="./images/star-icon.svg" allt="rating-icon" />
+              <span class="rating-value">${this.info.rating}</span>
+            </div>
+          </div>
+
+          <div class="sddres-info">
+            <div class="addres">
+              <span>${this.info.address}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+}
+
+customElements.define('restaurant-info', RestaurantInfo);
