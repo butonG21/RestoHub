72a150dddb2282e2811c1519be80d37fb8216251
butong <anggi21.af@gmail.com>
2023-11-03 01:01:01 +0700

    delete sw.js , cache-helper.js, cache_name, sw entry

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,5 @@
 const CONFIG = {
   BaseUrl: 'https://restaurant-api.dicoding.dev',
   BaseImage: 'https://restaurant-api.dicoding.dev/images',
-  CACHE_NAME: 'RestoHub-V1',
 };
 export default CONFIG;
