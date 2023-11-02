236faa2c2ff1587861b0e5604ad413e4581a3878
butong <anggi21.af@gmail.com>
2023-11-03 02:30:12 +0700

    set config idb

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,8 @@
 const CONFIG = {
   BaseUrl: 'https://restaurant-api.dicoding.dev',
   BaseImage: 'https://restaurant-api.dicoding.dev/images',
+  DATABASE_NAME: 'RestoHub-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurants',
 };
 export default CONFIG;
